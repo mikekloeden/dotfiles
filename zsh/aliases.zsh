b4eab8f1778fc8e314c14bfd894190f1167edded
Mike Klöden <mike.kloeden@gmail.com>
2012-11-15 09:56:00 +0100

    move directory completion functions to aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,3 +7,16 @@ hash -d github=~code/github
 
 hash -d git=/Users/Shared/development/projects_git
 hash -d mikl=~git/development/mikl
+
+# completion for some directories
+c() { cd $PROJECTS/$1; }
+_c() { _files -W $PROJECTS -/; }
+compdef _c c
+
+cdgit() { cd ~git/$1; }
+_cdgit() { _files -W ~git -/; }
+compdef _cdgit cdgit
+
+cdmp() { cd ~git/mediaport3/$1; }
+_cdmp() { _files -W ~git/mediaport3 -/; }
+compdef _cdmp cdmp
