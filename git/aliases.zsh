1ac89c7ffd40594ce1d1a6dc6c50186413fbab57
Mike Klöden <mike.kloeden@gmail.com>
2012-11-22 10:53:01 +0100

    change git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,6 +1,7 @@
 alias gcd='cd "`git rev-parse --show-toplevel`"'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gsb='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gs='git status'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias gp='git push origin HEAD'
 alias gd='git diff'
