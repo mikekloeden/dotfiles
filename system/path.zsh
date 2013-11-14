2997d3bb1c7d986e423990861bd9a13f15d0c195
Mike Klöden <mike.kloeden@gmail.com>
2013-11-14 14:23:45 +0100

    fix path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="/usr/local/git/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$ZSH/bin:./bin:/Developer/usr/bin:/usr/local/apache-maven/default/bin:/usr/local/mysql/bin:/usr/local/mongodb/default/bin:$PATH"
+export PATH="/usr/local/share/npm/bin:$ZSH/bin:./bin:$HOME/.gem/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
